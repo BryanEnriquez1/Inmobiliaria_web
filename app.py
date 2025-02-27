3a0f4f8cd86e5f3bebf2c4b04a671f7a96d43428
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2025-02-27 09:22:10 -0500

    Testing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='VISTAS')
 
 @app.route('/')
 def home():
-    return render_template('VISTAS/test.html')
+    return render_template('test.html')  # Cambiado a 'test.html'
 
 if __name__ == '__main__':
     app.run(debug=True)
