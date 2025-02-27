6df202f6dd8cdf1fc4d48971600d5186ebb579fa
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2025-02-27 09:26:32 -0500

    Testing Page info

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='VISTAS')
 
 @app.route('/')
 def home():
-    return render_template('test.html')  # Cambiado a 'test.html'
+    return render_template('inmueble_info.html')  # Cambiado a 'test.html'
 
 if __name__ == '__main__':
     app.run(debug=True)
