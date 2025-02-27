2c8367c2ae4b2a447ffe2a1b4e4329b2791132fc
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2025-02-27 09:17:57 -0500

    Correccion de web

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('VISTAS/inmueble_info.html')
+    return render_template('VISTAS/test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
